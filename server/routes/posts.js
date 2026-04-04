ca1c8c6444520652433008c133af14898dd3c738
Your Name <your-email@example.com>
2026-04-04 14:49:37 -0700

    add DELETE a post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -65,4 +65,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      'DELETE FROM posts WHERE id = $1 RETURNING *',
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+    res.json({ message: 'Post deleted sucessfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 export default router;
