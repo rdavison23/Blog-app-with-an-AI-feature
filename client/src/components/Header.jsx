d9341dbc227292502d9b73138ff5e0291caba7a2
Your Name <your-email@example.com>
2026-04-06 15:20:49 -0700

    add header style and import header into App

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="logo"> Fashion Problems No One Talks About </h1>
+      <h1 className="logo">Fashion Problems No One Talks About</h1>
       <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/new">New Post</Link>
